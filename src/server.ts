00a0c58452a74528f73c4de9f8639f52bd19d0eb
picakhant <picakhant@gmail.com>
2025-04-06 10:26:47 +0630

    feat: add admin-controlled Gmail registration for Google OAuth login

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
-import express from 'express'
+import express from "express";
+import oAuthRouter from "./routes/oauth";
+import cookieParser from "cookie-parser";
 
-const server  = express()
+const server = express();
 
-server.listen(3000, () => { 
-    console.log("Server is running!")
- })
+// for json response
+server.use(express.json());
+
+// to access server static html request data
+server.use(express.urlencoded({ extended: true }));
+
+// to send server static file
+server.use(express.static("view"));
+
+// toaccess cookie across on every router
+server.use(cookieParser());
+
+server.use("/api/auth", oAuthRouter);
+
+server.listen(3000, () => {
+  console.log("Server is running!");
+});
